9da9f5ec405d1080bcf393d8cd7d0d711965540a
Curt <Cphilpot798@gmail.com>
2019-04-25 06:07:45 -0400

    Lab 6

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Wave = 1;
+            while (Wave == 1)
+            {
+                RNDM.Roller();
+                Console.WriteLine("Would you like to go again? \nYah or Nah?");
+                Wave++;
+                string yes = null;
+                yes = Console.ReadLine();
+                if (yes == "Yah")
+                {
+                    Wave--;
+                }
+                if (yes == "Nah")
+                {
+                    Console.WriteLine("Hope You Have A Good Day!");
+                }
+
+            }
+            
+            
+        }
+    }
+}
---
+++ RNDM.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_6
+{
+    class RNDM
+    {
+        public static void Roller()
+        {
+            Console.WriteLine("Welcome High Roller! \nHow many sides are you rolling tonight?");
+
+            int Xsides = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ready To Roll? \nYah or Nah?");
+            string ready = Console.ReadLine();
+
+            Random Die = new Random();
+
+            int rolled = 2;
+            while (ready == "Yah" && rolled == 2 || rolled == 1)
+            {
+                int Win = Die.Next(1, Xsides);
+                Console.WriteLine(Win);
+                rolled--;
+            }
+        }
+    }
+}
